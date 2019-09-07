c71f5f10fe326b0e80146ac74955d201fe153e14
Alvaro <advillalba@outlook.com>
2019-09-07 12:51:27 +0200

    Añadido sonido de reloj durante el entrenamiento y ajustados textos

--- src/main/kotlin/com/healthylife/alexaskill/utils/Speeches.kt
+++ src/main/kotlin/com/healthylife/alexaskill/utils/Speeches.kt
@@ -8,11 +8,34 @@ class Speeches {
         const val repromptWelcome = "Puedes pedirme comenzar por un entrenamiento."
         const val helper = "¿Qué entrenamiento quieres realizar?"
         const val repromptHelper ="Podemos realizar un entrenamiento básico, medio o avanzado."
-        const val startExercice = "<prosody volume=\"x-loud\">5...4...3..2...1....</prosody>"
-        const val exerciceCounter = "<break time=\"10s\"/><break time=\"5s\"/><prosody volume=\"soft\">5...4...3...2...1....</prosody>"
-        const val lastExerciseCounter = "<break time=\"10s\"/><break time=\"5s\"/><prosody volume=\"soft\">5...4...3...2...1....</prosody><prosody volume=\"x-loud\">¡Terminamos!</prosody>"
-        const val breakCounter = "<break time=\"5s\"/><prosody volume=\"soft\">5...4...3...2...1....</prosody>"
-        const val goodbye = "¡<say-as interpret-as=\"interjection\">gracias</say-as> por usar vida saludable!<say-as interpret-as=\"interjection\">hasta luego</say-as>!"
+        const val startExercice = """ 
+             <prosody volume="x-loud">5...4...3..2...1....</prosody>
+             """
+
+        const val exerciceCounter = """ 
+                <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/><break strength="none"/>
+                <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/><break strength="none"/>
+                <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/><break strength="none"/>
+                <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/><break strength="none"/>
+                <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/><prosody volume="soft">5...4...3...2...1....</prosody>
+                """
+        const val lastExerciseCounter = """
+             <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/><break strength="none"/>
+                <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/><break strength="none"/>
+                <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/><break strength="none"/>
+                <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/><break strength="none"/>
+                <audio src="soundbank://soundlibrary/clocks/ticks/ticks_10"/>
+                <prosody volume="soft">5...4...3...2...1....</prosody>
+                <prosody volume="x-loud">¡Terminamos!</prosody>"
+             """
+
+        const val breakCounter = """
+                <break time="5s"/><prosody volume="x-loud">5...4...3...2...1....</prosody>
+                """
+
+        const val goodbye =  """
+                    ¡<say-as interpret-as="interjection">gracias</say-as> por usar vida saludable!<say-as interpret-as="interjection">hasta luego</say-as>!
+                """
 
     }
 }
